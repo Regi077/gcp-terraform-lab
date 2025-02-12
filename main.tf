0a7c7f5dac1ff7be3b165cdb06ca0839fea7b4e6
Regi077 <166327052+Regi077@users.noreply.github.com>
2025-02-12 17:06:00 -0500

    Update main.tf collect

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
+# Configure GCP Provider
 provider "google" {
   project = var.gcp_project
   region  = "us-central1"
 }
 
+# Enable Cloud Run API
 resource "google_project_service" "cloudrun" {
   service = "run.googleapis.com"
 }
 
+# Deploy Cloud Run Service
 resource "google_cloud_run_service" "default" {
   name     = "hello-world"
   location = "us-central1"
@@ -19,10 +22,10 @@ resource "google_cloud_run_service" "default" {
     }
   }
 
-
   depends_on = [google_project_service.cloudrun]
 }
 
+# Allow Public Access to Cloud Run
 resource "google_cloud_run_service_iam_member" "public_access" {
   location = google_cloud_run_service.default.location
   service  = google_cloud_run_service.default.name
@@ -30,18 +33,26 @@ resource "google_cloud_run_service_iam_member" "public_access" {
   member   = "allUsers"
 }
 
+# Assign Viewer Role to Your Email
 resource "google_project_iam_member" "cloud_run_viewer" {
   project = var.gcp_project
   role    = "roles/viewer"
-  member  = "user:cv.brainiac@gmail.com"
+  member  = "user:cv.brainiac@gmail.com"  # <-- YOUR EMAIL IS HERE
 }
 
+# Encrypt Terraform State in GCS Bucket
+terraform {
+  backend "gcs" {
+    bucket = "tf-state-REPLACE_WITH_YOUR_GCP_PROJECT_ID"  # Replace with your bucket name
+    prefix = "terraform/state"
+  }
+}
 
+# Variable for GCP Project ID
 variable "gcp_project" {
   description = "Your GCP Project ID"
   type        = string
 }
-
 
 terraform {
   backend "gcs" {
